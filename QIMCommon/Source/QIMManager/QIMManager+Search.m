7cdadd361a975622621712377e143c5c53321204
lilu <lilu@liludeMacBook-Pro.local>
2019-06-21 19:27:42 +0800

    update

--- QIMCommon/Source/QIMManager/QIMManager+Search.m
+++ QIMCommon/Source/QIMManager/QIMManager+Search.m
@@ -12,6 +12,11 @@
 
 - (void)searchWithUrl:(NSString *)url withParams:(NSDictionary *)params withSuccessCallBack:(QIMKitSearchSuccessBlock)successCallback withFaildCallBack:(QIMKitSearchFaildBlock)faildCallback {
     
+    NSInteger action = [[params objectForKey:@"action"] integerValue];
+    if (action == 8) {
+        url = @"https://03682da0-c9cb-4f53-a918-22903cb93bc3.mock.pstmn.io/search";
+    }
+    
     NSData *bodyData = [[QIMJSONSerializer sharedInstance] serializeObject:params error:nil];
     [self sendTPPOSTRequestWithUrl:url withRequestBodyData:bodyData withSuccessCallBack:^(NSData *responseData) {
         NSDictionary *responseDic = [[QIMJSONSerializer sharedInstance] deserializeObject:responseData error:nil];
